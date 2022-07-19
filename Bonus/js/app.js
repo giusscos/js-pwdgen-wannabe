96857246528ec7cef6e86b3541b3e9dc92e229e6
giusscos <giu.cosenza08@gmail.com>
2022-07-19 15:27:11 +0200

    Fix Name Request

--- Bonus/js/app.js
+++ Bonus/js/app.js
@@ -1,4 +1,5 @@
-const myName = prompt('Come ti chiami?');
+const myName = prompt('Qual e\' il tuo nome?');
+const mySurname = prompt('Qual e\' il tuo cognome?');
 const myAge = prompt('Quanti anni hai?');
 const myColor = prompt('Qual e\' il tuo colore preferito?');
 // let numGen = 10;
@@ -7,4 +8,4 @@ let numGen = Math.floor(Math.random() * 100).toString(); // Math.floor -> con la
 // console.log(typeof(numGen));
 // console.log(myName + myAge + myColor);
 
-document.getElementById('generator').innerHTML = 'La tua nuova password &eacute; &rightarrow; ' + myName + myAge + myColor + numGen;
+document.getElementById('generator').innerHTML = 'La tua nuova password &eacute; &rightarrow; ' + myName + mySurname + myAge + myColor + numGen;
--- js/app.js
+++ js/app.js
@@ -1,8 +1,9 @@
-const myName = prompt('Come ti chiami?');
+const myName = prompt('Qual e\' il tuo nome?');
+const mySurname = prompt('Qual e\' il tuo cognome?');
 const myAge = prompt('Quanti anni hai?');
 const myColor = prompt('Qual e\' il tuo colore preferito?');
 let numGen = 10;
 
 // console.log(myName + myAge + myColor);
 
-document.getElementById('generator').innerHTML = 'La tua nuova password &eacute; &rightarrow; ' + myName + myAge + myColor + numGen;
+document.getElementById('generator').innerHTML = 'La tua nuova password &eacute; &rightarrow; ' + myName + mySurname + myAge + myColor + numGen;
