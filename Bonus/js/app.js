d4c2f5e20f8bf4104152e6f951bdd39332e2e79e
giusscos <giu.cosenza08@gmail.com>
2022-07-19 15:22:23 +0200

    Add Bonus (CSS + Random Number Gen) + Minor Fix

--- Bonus/js/app.js
+++ Bonus/js/app.js
@@ -1,8 +1,10 @@
 const myName = prompt('Come ti chiami?');
 const myAge = prompt('Quanti anni hai?');
 const myColor = prompt('Qual e\' il tuo colore preferito?');
-let numGen = 10;
+// let numGen = 10;
+let numGen = Math.floor(Math.random() * 100).toString(); // Math.floor -> con la collaborazione di W3School <3
 
+// console.log(typeof(numGen));
 // console.log(myName + myAge + myColor);
 
 document.getElementById('generator').innerHTML = 'La tua nuova password &eacute; &rightarrow; ' + myName + myAge + myColor + numGen;
