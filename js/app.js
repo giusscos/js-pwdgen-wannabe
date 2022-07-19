c01ca75bc89b2ff63ca92081a571ca512940f6ff
giusscos <giu.cosenza08@gmail.com>
2022-07-19 15:00:48 +0200

    Add Number At The End Of  Password

--- js/app.js
+++ js/app.js
@@ -1,7 +1,8 @@
 const myName = prompt('Come ti chiami?');
 const myAge = prompt('Quanti anni hai?');
 const myColor = prompt('Qual e\' il tuo colore preferito?');
+let numGen = 10;
 
 // console.log(myName + myAge + myColor);
 
-document.getElementById('generator').innerHTML = 'La tua nuova password &eacute; >> ' + myName + myAge + myColor;
+document.getElementById('generator').innerHTML = 'La tua nuova password &eacute; >> ' + myName + myAge + myColor + numGen;
